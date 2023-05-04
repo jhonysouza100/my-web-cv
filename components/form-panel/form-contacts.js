d11a16f992455bfa884bf1ec86690edcfb5aaefc
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-05-04 06:18:00 -0300

    add error span

--- components/form-panel/form-contacts.js
+++ components/form-panel/form-contacts.js
@@ -1,28 +1,43 @@
 const d = document
 export default function contactsValidations(obj) {
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  const phoneRegex = /^[0-9]{10,11}$/
+
+  for(let el in obj){
+    if(obj[el].tagName === 'INPUT'){
+      const errorSpan = document.createElement('span');
+      errorSpan.classList.add('span_error');
+      errorSpan.innerText = 'Email incorrecto';
+      obj[el].insertAdjacentElement('afterend', errorSpan);
+    }  
+  }
+
   d.addEventListener('keyup', e => {
+
     let $ = e.target.value.toLowerCase().trim() || e.target.value.trim()
 
     if(e.target === obj.formEmail) {
-      const mensajeError = document.createElement("span");
-      mensajeError.textContent = "Por favor ingrese un correo electrónico válido";
-      mensajeError.classList.add("mensaje-error-email");
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
+      
       if(emailRegex.test(e.target.value)) {
         obj.profEmail.lastChild.data = $
-        document.parentNode.insertBefore(mensajeError, obj.formEmail);
         localStorage.setItem('email', $)
+        errorSpan.classList.remove('is-active')
       } else { 
-        console.error('Email inválido');
-        mensajeError.remove();
+        console.error('Email inválido')
+        errorSpan.classList.add('is-active')
       }
     } 
     
     if(e.target === obj.formPhone) {
-      obj.profPhone.lastChild.data = e.target.value.trim()
-      localStorage.setItem('phone', e.target.value.trim())
+      if(phoneRegex.test(e.target.value)) {
+        obj.profPhone.lastChild.data = e.target.value.trim()
+        localStorage.setItem('phone', e.target.value.trim())
+        errorSpan.classList.remove('is-active')
+      } else {
+        console.error('NÚmero inválido')
+        errorSpan.classList.add('is-active')
+      }
     }
       
     if(e.target === obj.formLinkedin) {
